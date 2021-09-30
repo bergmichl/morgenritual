dbc8bdea4c64ab563b9212bc9b8fec19d06d1c53
Michael Bergmüller <michaelbergmueller@hotmail.com>
2021-09-30 16:11:33 +0200

    added JS for page reminder

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// Page Reminder with cards
+// Page Reminder with random sentence (cards.json) showing
+
+window.addEventListener('load', showRandomSentence);
+
+function showRandomSentence(e) {
+  e.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+  
+  xhr.open('GET', 'cards.json', true);
+  
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const sentences = JSON.parse(this.responseText);
+      const num = randomNR(1, sentences.length);
+      const sentence = sentences[num].value;
+      
+      document.getElementById('sentence').innerText = sentence;
+    } else {
+      console.log(`Something went wrong while loading Data from cards.json! Status-code: ${this.status}`);
+    }
+  }
+
+  xhr.send();
+}
+
+// Get a random Nr. between 1 and cards.length
+function randomNR(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
